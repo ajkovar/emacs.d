a34073ff83897f6a2b77f0ee142fd6d0a2728838
Alex Kovar <ajkovar@gmail.com>
2014-02-05 11:49:58 -0600

    indent 4 spaces

--- init.el
+++ init.el
@@ -104,8 +104,8 @@
        (lambda (hook)
          (add-hook hook
                    (lambda ()
-                     (setq tab-width 4)
-                     (setq indent-tabs-mode t)))))
+                     (setq tab-width 4)))))
+(setq sgml-basic-offset 4)
 
 
 ;; disable backup
